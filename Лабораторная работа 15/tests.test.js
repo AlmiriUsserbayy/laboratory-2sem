03f13a8605ce8e044cc0e461c587fb74092f8064
AlmiriUsserbayy <almiri_001@inbox.ru>
2025-12-10 14:02:45 +0500

    Delete Лабораторная работа 15 directory

--- Лабораторная работа 15/tests.test.js
+++
@@ -1,7 +0,0 @@
-import { saveTasks, loadTasks } from "./storage.js";
-
-test("Сохранение и загрузка списка работает", () => {
-  const sample = ["task1", "task2"];
-  saveTasks(sample);
-  expect(loadTasks()).toEqual(sample);
-});
--- Лабораторная работа 15/utils.js
+++
@@ -1,5 +0,0 @@
-export function filterTasks(tasks, keyword) {
-  return tasks.filter(t =>
-    t.toLowerCase().includes(keyword.toLowerCase())
-  );
-}
